e20496ee7a7170cf36b3c6c4eb8838bb478d7312
duyanhhoangquoc <dh6k@outlook.com.vn>
2025-09-11 09:29:06 +0700

    testbench

--- bai2/testbench.v
+++ bai2/testbench.v
@@ -14,14 +14,14 @@ module testbench;
     //test cases
     initial begin
         //not overflow
-        a=4'd0;
-        b=4'd0;
+        a=4'd0;     //4'b0000
+        b=4'd0;     //4'b0000
         #10;
         a=4'd1;
         b=4'd1;
         #20;
-        a=4'd15;
-        b=4'd15;
+        a=4'd15;     //4'b1111
+        b=4'd15;     //4'b1111
     end
 
     initial begin
